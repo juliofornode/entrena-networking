41cfd631fa9373a8f93d06621aebcf20d4acf8c1
Julio <juliofornode@gmail.com>
2016-03-08 08:35:41 -0800

    ui router installer, home route set

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', []);
+angular.module('myApp', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state('home', {
+                url: "/",
+                templateUrl: "partials/home.html"
+            })
+    });
